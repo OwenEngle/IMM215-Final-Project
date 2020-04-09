7b8eafa63399008d0a1c60c923d47e24074c891a
Way Robertson <mizepie@hotmail.com>
2020-04-09 16:18:38 -0400

    Way: Scripts Folder+Streaming Handler Script
    This contains the script that handles streaming audio (work in progress).

---
+++ Assets/Scripts/AudioStreamingHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+
+public class AudioStreamingHandler : MonoBehaviour
+{
+ public AudioSource audioSource;   
+    void Start()
+    {
+        StartCoroutine(GetAudioClip());
+    }
+
+    public IEnumerator GetAudioClip()
+    {
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("http://www.teiubescrecords.com:8000/chill.ogg", AudioType.MPEG))
+        {
+            www.SendWebRequest();
+            while(!www.isDone)
+            {
+                Debug.Log(www.downloadedBytes);
+                yield return null;
+            }
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                AudioClip streamedAudio = DownloadHandlerAudioClip.GetContent(www);
+                audioSource.clip = streamedAudio;
+                audioSource.Play();
+            }
+        }
+    }
+}
