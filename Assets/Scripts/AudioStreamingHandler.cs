1c06f5827e2908099023f03928b23e7de3da8009
Way Robertson <mizepie@hotmail.com>
2020-04-11 18:49:29 -0400

    MusicBox_Working _Play_Back
    I added a URL that downloads from the web into the AudioStreamingHandler script.

--- Assets/Scripts/AudioStreamingHandler.cs
+++ Assets/Scripts/AudioStreamingHandler.cs
@@ -13,7 +13,7 @@ public class AudioStreamingHandler : MonoBehaviour
 
     public IEnumerator GetAudioClip()
     {
-        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("http://www.teiubescrecords.com:8000/chill.ogg", AudioType.MPEG))
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("https://ia803005.us.archive.org/11/items/soundcloud-557089965/No_Copyright_Music_Chill_Lofi_Hip_Hop_Instrumental_Copyright_Free_Music_-_Days_Like_These-557089965.ogg", AudioType.OGGVORBIS))
         {
             www.SendWebRequest();
             while(!www.isDone)
