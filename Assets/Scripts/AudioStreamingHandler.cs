00b799e7040892f431ff30028903a639bf815322
Way Robertson <mizepie@hotmail.com>
2020-04-20 15:48:13 -0400

    Updated Streaming Handler
    the streaming handler can be used as a playlist.

--- Assets/Scripts/AudioStreamingHandler.cs
+++ Assets/Scripts/AudioStreamingHandler.cs
@@ -2,35 +2,77 @@
 using UnityEngine.Networking;
 using System.Collections;
 
-
 public class AudioStreamingHandler : MonoBehaviour
 {
- public AudioSource audioSource;   
+    public AudioSource audioSource;
+    public int songIndex;
+    public string[] songUrls;
+    bool playing = true;
+    //public string Song1 = "https://ia803005.us.archive.org/11/items/soundcloud-557089965/No_Copyright_Music_Chill_Lofi_Hip_Hop_Instrumental_Copyright_Free_Music_-_Days_Like_These-557089965.ogg";
     void Start()
     {
+        songIndex = 0;
         StartCoroutine(GetAudioClip());
     }
 
     public IEnumerator GetAudioClip()
     {
-        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("https://ia803005.us.archive.org/11/items/soundcloud-557089965/No_Copyright_Music_Chill_Lofi_Hip_Hop_Instrumental_Copyright_Free_Music_-_Days_Like_These-557089965.ogg", AudioType.OGGVORBIS))
+        while (playing)
         {
-            www.SendWebRequest();
-            while(!www.isDone)
+            string url = songUrls[songIndex];
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.OGGVORBIS))
             {
-                Debug.Log(www.downloadedBytes);
-                yield return null;
+                www.SendWebRequest();
+                while (!www.isDone)
+                {
+                    Debug.Log(www.downloadedBytes);
+                    yield return null;
+                }
+                if (www.isNetworkError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    AudioClip streamedAudio = DownloadHandlerAudioClip.GetContent(www);
+                    audioSource.clip = streamedAudio;
+                    audioSource.Play();
+                    yield return new WaitForSeconds(audioSource.clip.length);
+                }
+
             }
-            if (www.isNetworkError)
+
+            songIndex++;
+            if (songIndex >= songUrls.Length)
             {
-                Debug.Log(www.error);
+                songIndex = 0;
             }
-            else
-            {
-                AudioClip streamedAudio = DownloadHandlerAudioClip.GetContent(www);
-                audioSource.clip = streamedAudio;
-                audioSource.Play();
-            }
+
         }
+    }
+    IEnumerator playList()
+    {
+        // for( int i = 0; i < songUrl.Length; i++)
+        // {
+        //     songUrl[0];
+        //     Debug.Log("waiting");
+        //     yield return new WaitForSeconds(2);
+        //     songUrl[1];
+        //     Debug.Log("waiting");
+        //     yield return new WaitForSeconds(2)
+        //     songUrl[2];
+        //     Debug.Log("waiting");
+
+
+
+        // // = "https://ia803005.us.archive.org/11/items/soundcloud-557089965/No_Copyright_Music_Chill_Lofi_Hip_Hop_Instrumental_Copyright_Free_Music_-_Days_Like_These-557089965.ogg";
+        // yield return WaitForSeconds(240);
+
+        // string Song2 
+        // //= "https://ia803005.us.archive.org/11/items/soundcloud-557089965/No_Copyright_Music_Chill_Lofi_Hip_Hop_Instrumental_Copyright_Free_Music_-_Days_Like_These-557089965.ogg";
+        // yield return WaitForSeconds(240);
+        //dillinger markdown website
+        //}
+        yield return null;
     }
 }
