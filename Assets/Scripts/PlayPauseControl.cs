4968e81a756b1f332353a79dc1d6071e96dcdf5b
dylanboswell <dylanboswell22@gmail.com>
2020-04-20 19:53:14 -0400

    Updates to increase testability of play/pause

--- Assets/Scripts/PlayPauseControl.cs
+++ Assets/Scripts/PlayPauseControl.cs
@@ -7,41 +7,56 @@ public class PlayPauseControl : MonoBehaviour
     public AudioSource radio;
     public float muteVolume;
     public bool muted;
+    public bool muteUpdate;
 
     // Start is called before the first frame update
     void Start()
     {
         muteVolume = radio.volume;
         muted = false;
+        muteUpdate = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetButtonDown("XRI_Left_SecondaryButton") || Input.GetButtonDown("XRI_Right_SecondaryButton")) /*b or y*/
-        { 
-            if(muted == false)
+        if (muteUpdate == true)
+        {
+            if (muted == true)
             {
                 muteVolume = radio.volume;
                 radio.volume = 0;
+                muteUpdate = false;
             }
-            else
-            {
-                return;
-            }
-           
-        }
-        if (Input.GetButtonDown("XRI_Left_PrimaryButton") || Input.GetButtonDown("XRI_Right_PrimaryButton")) /*a or x*/
-        {   
-            if(muted == true)
+            else if (muted == false)
             {
                 radio.volume = muteVolume;
+                muteUpdate = false;
             }
             else
             {
                 return;
             }
+        }
+        else
+        {
+            return;
+        }
+
+
+
+        if (Input.GetButtonDown("XRI_Left_SecondaryButton") || Input.GetButtonDown("XRI_Right_SecondaryButton")) /*b or y*/
+        {
+            muteUpdate = true;
+            muted = false;
+            
+
+        }
+        if (Input.GetButtonDown("XRI_Left_PrimaryButton") || Input.GetButtonDown("XRI_Right_PrimaryButton")) /*a or x*/
+        {
+            muteUpdate = true;
+            muted = true;
+            
             
         }
     }
